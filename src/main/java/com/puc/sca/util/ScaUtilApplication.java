ad087cd6b24bba8a84e6d59764875fbb5e20c946
breno500as <breno500as@github.com>
2020-05-11 12:00:01 -0300

    atualizando o spring boot

--- src/main/java/com/puc/sca/util/ScaUtilApplication.java
+++ src/main/java/com/puc/sca/util/ScaUtilApplication.java
@@ -12,11 +12,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 
  * Passos para dar deploy de uma dependência (package) no github:
  * 1 - Gerar um token de autorização de acesso na conta do github (o token deve ter as permissões read:packages e write:packages): https://help.github.com/pt/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
- * 2 - Configurar no settings.xml do maven as propriedades: server (com usuário e o token de acesso) e um profile chamado github com o endereço do repositório remoto do github
- * 3 - Configurar no pom da aplicação o distributionManagement apontando para o endereço do repositório remoto do github
+       e configurar no settings.xml do maven
+       
+     <server>
+      <id>github</id>
+      <username>breno500as</username>
+      <password>9ca4f8d61e8491df0f6b55f74c604ade3b0a5e9a</password>
+     </server>
+ 
+ 
+ * 2 - Configurar no settings.xml do maven as propriedades: server (com usuário e o token de acesso) e em cada profile da aplicação apontar também o repositório com o endereço do repositório remoto do github
+ * 
+ *  <profile>
+      <id>hml</id>
+      <repositories>
+        <repository>
+          <id>central</id>
+          <url>https://repo1.maven.org/maven2</url>
+          <releases><enabled>true</enabled></releases>
+          <snapshots><enabled>true</enabled></snapshots>
+        </repository>
+        <repository>
+          <id>github</id>
+          <name>GitHub OWNER Apache Maven Packages</name>
+          <url>https://maven.pkg.github.com/breno500as/sca-util</url>
+          <releases><enabled>true</enabled></releases>
+          <snapshots><enabled>true</enabled></snapshots>
+        </repository>
+      </repositories>
+    </profile>
+ *  
+ * 3 - Configurar no pom dessa aplicação o distributionManagement apontando para o endereço do repositório remoto do github
  * 4 - Executar o deploy da aplicação maven (por linha de comando ou pela ide do eclipse): mvn deploy -Dregistry=https://maven.pkg.github.com/breno500as/sca-util -Dtoken=$3469dd9725e55914e6d98a8236b14215842d8c07
- * 5 - As aplicações que desarem instalar essa dependencia devem ter a dependencia declarada no pom e o install deve utilizar o profile  do github: Ex: mvn install -P github
- * 6 - Sempre que for necessário atualizar a aplicação deve se atualizar a <version> da dependência no pom.xml e executar o passo 4 novamente
+ * 5 - Sempre que for necessário atualizar a aplicação deve se atualizar a <version> da dependência no pom.xml e executar o passo 4 novamente
  * 
  * Exemplos:
  * https://help.github.com/pt/packages/using-github-packages-with-your-projects-ecosystem/configuring-apache-maven-for-use-with-github-packages

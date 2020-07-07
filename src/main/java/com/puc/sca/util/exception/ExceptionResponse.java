a9a82862e3ae01d208d850e22c7f43981bd9d75f
breno500as <breno500as@gmail.com>
2020-07-07 19:38:22 -0300

    data

--- src/main/java/com/puc/sca/util/exception/ExceptionResponse.java
+++ src/main/java/com/puc/sca/util/exception/ExceptionResponse.java
@@ -1,43 +1,46 @@
 package com.puc.sca.util.exception;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class ExceptionResponse implements Serializable {
-	
 
 	private static final long serialVersionUID = -467333885245388059L;
 
-	private Date timestamp;
-	
+	private LocalDateTime timestamp;
+
 	private Object[] messages;
-	
+
 	private String details;
-	
-	public ExceptionResponse(Date timestamp, String details, Object ... messages) {
+
+	public ExceptionResponse(LocalDateTime timestamp, String details, Object... messages) {
 		this.timestamp = timestamp;
 		this.details = details;
 		this.messages = messages;
 	}
 
-	public Date getTimestamp() {
+	public LocalDateTime getTimestamp() {
 		return timestamp;
+	}
+
+	public void setTimestamp(LocalDateTime timestamp) {
+		this.timestamp = timestamp;
 	}
 
 	public Object[] getMessages() {
 		return messages;
 	}
-	
+
 	public void setMessages(Object[] messages) {
 		this.messages = messages;
 	}
-	
+
 	public String getDetails() {
 		return details;
 	}
-	
+
 	public void setDetails(String details) {
 		this.details = details;
 	}
-	
+
 }
